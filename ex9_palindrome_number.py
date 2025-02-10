91d8a9243e23b552027cec4ee2318fe38bdf2b6d
tpratap <tripuraripratap@gmail.com>
2025-02-10 05:03:37 +0000

    leetcode ex9_palindrome_number

--- ex9_palindrome_number.py
+++ ex9_palindrome_number.py
@@ -1,3 +1,31 @@
+"""
+Ex 9: Palindrome Number
+
+Given an integer x, return true if x is a palindrome, and false otherwise.
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ 
+Constraints:
+
+-231 <= x <= 231 - 1
+ 
+Follow up: Could you solve it without converting the integer to a string?
+"""
+
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0: return False
@@ -5,15 +33,13 @@ class Solution:
         div = 1
         while x >= 10 * div:
             div *= 10
+        print("<Value of x , div is %s and %s >" % (x, div))
 
         while x:
-            right = x % 10
-            left = x // div
-
-            if left != right: return False
-
+            if x // div != x % 10: return False
             x = (x % div) // 10
             div = div / 100
+            print("<Value of x , div is %s and %s >" % (x, div))
         return True
 
 instance_Solution = Solution()
